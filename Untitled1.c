217333f32046236ca5f8ac3c1373a2b360819d4c
Bhagyachi <92204095+Bhagyachi@users.noreply.github.com>
2021-11-10 15:29:25 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,res=0;
+	scanf("%d",&n);
+	res=rec();
+	printf("%d",res);
+}
+ rec(int i)
+{
+	int n;
+	while(i<=n)
+	if(i==0)
+	{
+		return 0;
+	}
+	else
+	{
+		return n ;
+	}
+}
