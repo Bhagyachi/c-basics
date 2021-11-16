6553bdc5b97b1c87757da969834b68aac9dff286
Bhagyachi <92204095+Bhagyachi@users.noreply.github.com>
2021-11-16 15:15:29 +0530

    Add files via upload

---
+++ matrix array.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void main()
+{
+	int a[10][10],b[10][10],i,j,sum=0;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+{
+	scanf("%d",&a[i][j]);
+}
+printf("\n");
+}
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+	printf("%d",a[i][j]);
+}
+		printf("\n");	
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d",b[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			sum=a[i][j]+b[i][j];
+			printf("%d",sum);
+		}
+		printf("\n");
+	}
+}
+
